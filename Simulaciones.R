27a4f843b758b8443df3df244640b667738f4092
josegomez9203 <70537677+josegomez9203@users.noreply.github.com>
2022-05-02 11:53:50 -0500

    Add files via upload

--- Simulaciones.R
+++ Simulaciones.R
@@ -11,6 +11,9 @@ library(RTransferEntropy)
 library(vars)
 library(mvtnorm)
 library(lmtest)
+library(dplyr)
+library(tidyr)
+library(data.table)
 ############### 1 ###########
 
 Sistema_1 <- function(n, semilla = 12345){
@@ -68,7 +71,6 @@ grafica_4var <- function(Database, Sistema ,type ="l"){
   legend("topright", legend = c("x1","x2","x3","x4"),
          lwd=3, col = c("black","red","darkblue","darkgreen"))
 }
-
 
 
 Wavelet_3 <- function(Database,nrands = 200){
@@ -292,17 +294,19 @@ TE4_sbt <- function(Database,entropy = 'Shannon',shuffles = 200, semilla = 12345
 varfit <- VAR(cbind(x1,x2),p=1,type = 'const')
 sum_var =summary(varfit)
 sum_var$varresult$x1
+VAR(select(as.data.frame(Database),3,1),p=1,type = 'const')
 
 coef.transfer_entropy(A,B)
-
-
+select(Database,1,2)
+Database
 # SIMULACION SISTEMA 1
-Database <- Sistema_1(200)
+Database <- Sistema_1(2048)
 grafica_3var(Database,"1")
 Wavelet_3(Database)
 TE3_boot(Database)
 TE3_sbt(Database)
 
+fwrite(Database,file="F:/Tarea/TESIS/IMAGENES/Sistema_1_2048.csv")
 
 ############### 2 ###########
 
@@ -347,12 +351,13 @@ coef.transfer_entropy(A,B)
 
 
 # SIMULACION SISTEMA 2
-Database <- Sistema_2(200)
+Database <- Sistema_2(2048)
 grafica_3var(Database,"2")
 Wavelet_3(Database)
 TE3_boot(Database)
 TE3_sbt(Database)
 
+fwrite(Database,file="F:/Tarea/TESIS/IMAGENES/Sistema_2_2048.csv")
 
 ############### 3 ###########
 
@@ -393,13 +398,13 @@ coef.transfer_entropy(A,B)
 
 
 # SIMULACION SISTEMA 3
-Database <- Sistema_3(200)
+Database <- Sistema_3(2048)
 grafica_3var(Database,"3")
 Wavelet_3(Database)
 TE3_boot(Database)
 TE3_sbt(Database)
 
-
+fwrite(Database,file="F:/Tarea/TESIS/IMAGENES/Sistema_3_2048.csv")
 
 ############### 4 ###########
 
@@ -443,14 +448,14 @@ coef.transfer_entropy(A,B)
 
 
 # SIMULACION SISTEMA 1
-Database <- Sistema_4(200)
+Database <- Sistema_4(2048)
 grafica_4var(Database,"4")
 Wavelet_4(Database)
 TE4_boot(Database)
 TE4_sbt(Database)
 
 
-
+fwrite(Database,file="F:/Tarea/TESIS/IMAGENES/Sistema_4_2048.csv")
 
 ############### 5 ########### 
 
@@ -484,13 +489,13 @@ coef.transfer_entropy(A,B)
 
 
 # SIMULACION SISTEMA 5
-Database <- Sistema_5(200)
+Database <- Sistema_5(2048)
 grafica_3var(Database,"5")
 Wavelet_3(Database)
 TE3_boot(Database)
 TE3_sbt(Database)
 
-
+fwrite(Database,file="F:/Tarea/TESIS/IMAGENES/Sistema_5_2048.csv")
 
 ############### 6 ###########
 
@@ -498,13 +503,17 @@ Sistema_6 <- function(n,ac = 0.5 , semilla = 12345){
   
   xM = Sistema_3(n,ac,semilla)
   ind = 0 
-  n_t = c(0)
+  n_t1 = c(0)
+  n_t2 = c(0)
+  n_t3 = c(0)
   while (ind==0) {
     for( i in seq(2,n) ){
-      n_t[i] = (n_t[i-1] + rnorm(1))
-      xM[i,1] = xM[i,1] + n_t[i]
-      xM[i,2] = xM[i,2] + n_t[i]
-      xM[i,3] = xM[i,3] + n_t[i]
+      n_t1[i] = (n_t1[i-1] + rnorm(1))
+      n_t2[i] = (n_t2[i-1] + rnorm(1))
+      n_t3[i] = (n_t3[i-1] + rnorm(1))
+      xM[i,1] = xM[i,1] + n_t1[i]
+      xM[i,2] = xM[i,2] + n_t2[i]
+      xM[i,3] = xM[i,3] + n_t3[i]
     }
     if(is.infinite(sum(sum(xM)))||is.nan(sum(sum(xM)))){
       ind=0
@@ -525,12 +534,13 @@ coef.transfer_entropy(A,B)
 
 
 # SIMULACION SISTEMA 6
-Database <- Sistema_6(200)
+Database <- Sistema_6(2048)
 grafica_3var(Database,"6")
 Wavelet_3(Database)
 TE3_boot(Database)
 TE3_sbt(Database)
 
+fwrite(Database,file="F:/Tarea/TESIS/IMAGENES/Sistema_6_2048.csv")
 
 ############### 7 ###########
 
@@ -539,9 +549,9 @@ Sistema_7 <- function(n,ac = 0.5 , semilla = 12345){
   ind = 0 
   
   while (ind==0) {
-    a1 = rnorm(n,0.01,0.02)
-    a2 = rnorm(n,0.01,0.02)
-    a3 = rnorm(n,0.01,0.02)
+    a1 = rnorm(1,0.01,0.02)
+    a2 = rnorm(1,0.01,0.02)
+    a3 = rnorm(1,0.01,0.02)
     t = seq(1,n)
     n_t1 = a1*t
     n_t2 = a2*t
@@ -570,12 +580,13 @@ coef.transfer_entropy(A,B)
 
 
 # SIMULACION SISTEMA 7
-Database <- Sistema_7(200)
+Database <- Sistema_7(2048)
 grafica_3var(Database,"7")
 Wavelet_3(Database)
 TE3_boot(Database)
 TE3_sbt(Database)
 
+fwrite(Database,file="F:/Tarea/TESIS/IMAGENES/Sistema_7_2048.csv")
 
 ############### 8 ###########
 
@@ -587,23 +598,26 @@ Sistema_8 <- function(n, semilla = 12345,a0 = 0.2,a1 = 0.9,b1 = 0.1,g= 1){
   
   set.seed(semilla)
   
-  Error = rmvnorm(1,mean = media,sigma = sigma)
-  Error2 = rmvnorm(1,mean = media,sigma = sigma)
+  # Error = rmvnorm(1,mean = media,sigma = sigma)
+  # Error2 = rmvnorm(1,mean = media,sigma = sigma)
+  # 
+  Error = rnorm(1)
+  Error2 = rnorm(1)
   
-  error1 = c(Error[1],Error2[1])
-  error2 = c(Error[2],Error2[2])
-  error3 = c(Error[3],Error2[3])
+  error1 = c(Error,Error2)
+  error2 = c(Error,Error2)
+  error3 = c(Error,Error2)
   
   
-  Database = Sistema_2((n),semilla)
+  Database = Sistema_2(n,semilla)
   
-  sigma2_1 = c(a0,a0+(a1*Error2[1]^2)+b1*a0)
-  sigma2_2 = c(a0,a0+(a1*Error2[2]^2)+b1*a0)
-  sigma2_3 = c(a0,a0+(a1*Error2[3]^2)+b1*a0)
+  sigma2_1 = c(a0,a0+(a1*Error2^2)+b1*a0)
+  sigma2_2 = c(a0,a0+(a1*Error2^2)+b1*a0)
+  sigma2_3 = c(a0,a0+(a1*Error2^2)+b1*a0)
   
-  zt1 = c(sigma2_1[1]*Error[1],sigma2_1[2]*Error2[1])
-  zt2 = c(sigma2_2[1]*Error[2],sigma2_2[2]*Error2[2])
-  zt3 = c(sigma2_3[1]*Error[3],sigma2_3[2]*Error2[3])
+  zt1 = c(sigma2_1[1]*Error,sigma2_1[2]*Error2)
+  zt2 = c(sigma2_2[1]*Error,sigma2_2[2]*Error2)
+  zt3 = c(sigma2_3[1]*Error,sigma2_3[2]*Error2)
   
   y1 = c(Database[1,1]+g*zt1[1],Database[2,1]+g*zt1[2])
   y2 = c(Database[1,2]+g*zt2[1],Database[2,2]+g*zt2[2])
@@ -612,11 +626,11 @@ Sistema_8 <- function(n, semilla = 12345,a0 = 0.2,a1 = 0.9,b1 = 0.1,g= 1){
   
   while (ind==0) {
     for( i in seq(2,(n-1)) ){
-      Error = rmvnorm(1,mean = media,sigma = sigma)
-      
-      error1[i+1] = Error[1]
-      error2[i+1] = Error[2]
-      error3[i+1] = Error[3]
+      #Error = rmvnorm(1,mean = media,sigma = sigma)
+      Error = rnorm(1)
+      error1[i+1] = Error
+      error2[i+1] = Error
+      error3[i+1] = Error
       
       sigma2_1[i+1] = a0 + a1*(error1[i]^2) + b1*(sigma2_1[i]^2)
       sigma2_2[i+1] = a0 + a1*(error2[i]^2) + b1*(sigma2_2[i]^2)
@@ -626,9 +640,9 @@ Sistema_8 <- function(n, semilla = 12345,a0 = 0.2,a1 = 0.9,b1 = 0.1,g= 1){
       zt2[i+1] = sigma2_2[i+1]*error2[i+1]
       zt3[i+1] = sigma2_3[i+1]*error3[i+1]
       
-      y1[i+1] = Database[i,1]+g*zt1[i+1]
-      y2[i+1] = Database[i,2]+g*zt2[i+1]
-      y3[i+1] = Database[i,3]+g*zt3[i+1]
+      y1[i+1] = Database[i+1,1]+g*zt1[i+1]
+      y2[i+1] = Database[i+1,2]+g*zt2[i+1]
+      y3[i+1] = Database[i+1,3]+g*zt3[i+1]
     }
     xM = c(y1,y2,y3)
     xM = matrix(xM,(n),3)
@@ -654,105 +668,47 @@ coef.transfer_entropy(A,B)
 
 
 # SIMULACION SISTEMA 8
-Database <- Sistema_8(500)
+Database <- Sistema_8(2048)
 grafica_3var(Database,"8")
 Wavelet_3(Database)
 TE3_boot(Database)
 TE3_sbt(Database)
 
 
+fwrite(Database,file="F:/Tarea/TESIS/IMAGENES/Sistema_8_2048.csv")
 
 
+############### 9 ###########
 
-
-
-
-### Wavelet Coherence
-# Se trata como serie
-A= cbind(rep(1:length(y1)),y1)
-B= cbind(rep(1:length(y2)),y2)
-C= cbind(rep(1:length(y3)),y3)
-
-nrands = 200 # Numero de aleatorizaciones montecarlo
-wtc.AB = wtc(A, B, nrands = nrands)
-wtc.AC = wtc(A, C, nrands = nrands)
-wtc.BC = wtc(B, C, nrands = nrands)
-
-
-# Graficamos
-par(mfrow=c(1,1),oma = c(0, 0, 0, 1), mar = c(5, 4, 5, 5) + 0.1)
-
-plot(wtc.AB, plot.phase = TRUE, lty.coi = 1, col.coi = "grey", lwd.coi = 1, 
-     lwd.sig = 2, arrow.lwd = 0.06, arrow.len = 0.15, ylab = "Escala", xlab = "Periodo", 
-     plot.cb = TRUE, main = "Wavelet Coherence: Y1 vs Y2")
-n = length(A[, 1])
-abline(v = seq(100, n, 100), h = 1:16, col = "darkgrey", lty = 1, lwd = 1)
-
-plot(wtc.AC, plot.phase = TRUE, lty.coi = 1, col.coi = "grey", lwd.coi = 2, 
-     lwd.sig = 2, arrow.lwd = 0.06, arrow.len = 0.15, ylab = "Escala", xlab = "Periodo", 
-     plot.cb = TRUE, main = "Wavelet Coherence: Y1 vs Y3")
-abline(v = seq(100, n, 100), h = 1:16, col = "darkgrey", lty = 1, lwd = 1)
-
-plot(wtc.BC, plot.phase = TRUE, lty.coi = 1, col.coi = "grey", lwd.coi = 2, 
-     lwd.sig = 2, arrow.lwd = 0.06, arrow.len = 0.15, ylab = "Escala", xlab = "Periodo", 
-     plot.cb = TRUE, main = "Wavelet Coherence: Y2 vs Y3")
-abline(v = seq(100, n, 100), h = 1:16, col = "darkgrey", lty = 1, lwd = 1)
-
-
-#### Transferencia de entropia
-transfer_entropy(y1, y2,q = 1, entropy = c('Shannon'), shuffles = 200, type = c('quantiles'),
-                 quantiles = c(5, 95), nboot = 300, burn = 60, seed = NULL)
-
-transfer_entropy(y1, y3,q = 1, entropy = c('Shannon'), shuffles = 200, type = c('quantiles'),
-                 quantiles = c(5, 95), nboot = 300, burn = 60, seed = NULL)
-
-transfer_entropy(y2, y3,q = 1, entropy = c('Shannon'), shuffles = 200, type = c('quantiles'),
-                 quantiles = c(5, 95), nboot = 300, burn = 60, seed = NULL)
-
-#Calculo de transferencia de entropía sin usar bootstrap
-#calc_ete(y1,y2) # Transferencia de entropía efectiva
-calc_te(y1,y2)
-calc_te(y2,y1)
-
-#calc_ete(y1,y3) # Transferencia de entropía efectiva
-calc_te(y1,y3)
-calc_te(y3,y1)
-
-#calc_ete(y3,y2) # Transferencia de entropía efectiva
-calc_te(y3,y2)
-calc_te(y2,y3)
-
-
-############### 10 ###########
-
-Sistema_10 <- function(n,g=0.2, semilla = 12345,a0 = 0.2,a1 = 0.9,b1 = 0.1){
+Sistema_9 <- function(n,g=0.2, semilla = 12345,a0 = 0.2,a1 = 0.9,b1 = 0.1){
   ind = 0
   
+  media <- c(0,0,0)
+  sigma<-diag(3)
+  set.seed(semilla)
   x1 = runif(3)
   x2 = runif(3)
   x3 = runif(3)
   
-  ERROR = rmvnorm(1,mean = media,sigma = sigma)
-  ERROR2 = rmvnorm(1,mean = media,sigma = sigma)
-  ERROR3 = rmvnorm(1,mean = media,sigma = sigma)
+  ERROR =  rnorm(1)
+  ERROR2 = rnorm(1)
+  ERROR3 = rnorm(1)
   
-  sigma2_1 = c(a0,a0+(a1*ERROR[1]^2)+b1*a0, a0+(a1*ERROR2[1]^2)+b1*(a0+(a1*ERROR[1]^2)+b1*a0))
-  sigma2_2 = c(a0,a0+(a1*ERROR[2]^2)+b1*a0, a0+(a1*ERROR2[2]^2)+b1*(a0+(a1*ERROR[2]^2)+b1*a0))
-  sigma2_3 = c(a0,a0+(a1*ERROR[3]^2)+b1*a0, a0+(a1*ERROR2[3]^2)+b1*(a0+(a1*ERROR[3]^2)+b1*a0))
+  sigma2_1 = c(a0,a0+(a1*ERROR^2)+b1*a0, a0+(a1*ERROR2^2)+b1*(a0+(a1*ERROR3^2)+b1*a0))
+  sigma2_2 = c(a0,a0+(a1*ERROR^2)+b1*a0, a0+(a1*ERROR2^2)+b1*(a0+(a1*ERROR3^2)+b1*a0))
+  sigma2_3 = c(a0,a0+(a1*ERROR^2)+b1*a0, a0+(a1*ERROR2^2)+b1*(a0+(a1*ERROR3^2)+b1*a0))
   
-  zt1 = c(sigma2_1[1]*ERROR[1],sigma2_1[2]*ERROR2[1],sigma2_1[3]*ERROR3[1])
-  zt2 = c(sigma2_2[1]*ERROR[2],sigma2_2[2]*ERROR2[2],sigma2_2[3]*ERROR3[2])
-  zt3 = c(sigma2_3[1]*ERROR[3],sigma2_3[2]*ERROR2[3],sigma2_3[3]*ERROR3[3])
+  zt1 = c(sigma2_1[1]*ERROR,sigma2_1[2]*ERROR2,sigma2_1[3]*ERROR3)
+  zt2 = c(sigma2_2[1]*ERROR,sigma2_2[2]*ERROR2,sigma2_2[3]*ERROR3)
+  zt3 = c(sigma2_3[1]*ERROR,sigma2_3[2]*ERROR2,sigma2_3[3]*ERROR3)
   
   y1 = c(x1[1]+g*zt1[1],x1[2]+g*zt1[2],x1[3]+g*zt1[3])
   y2 = c(x2[1]+g*zt2[1],x2[2]+g*zt2[2],x2[3]+g*zt2[3])
   y3 = c(x3[1]+g*zt3[1],x3[2]+g*zt3[2],x3[3]+g*zt3[3])
   
-  error1=c(ERROR[1],ERROR2[1],ERROR3[1])
-  error2=c(ERROR[2],ERROR2[2],ERROR3[2])
-  error3=c(ERROR[3],ERROR2[3],ERROR3[3])
-  rango = seq(700,from = 3)
-  i=3
+  error1=c(ERROR,ERROR2,ERROR3)
+  error2=c(ERROR,ERROR2,ERROR3)
+  error3=c(ERROR,ERROR2,ERROR3)
   
   while (ind==0) {
     for(i in seq(3,(n-1))  ){
@@ -762,10 +718,10 @@ Sistema_10 <- function(n,g=0.2, semilla = 12345,a0 = 0.2,a1 = 0.9,b1 = 0.1){
       x2[i+1] = 0.6*x2[i] + 0.2*x2[i-1] + 0.2*x2[i-2] + RuidoBlanco[2]
       x3[i+1] = 0.4*x3[i] + 0.3*x3[i-1] + 0.3*x3[i-2] + RuidoBlanco[3]
       
-      Error = rmvnorm(1,mean = media,sigma = sigma)
-      error1[i+1] = Error[1]
-      error2[i+1] = Error[2]
-      error3[i+1] = Error[3]
+      Error = rnorm(1)
+      error1[i+1] = Error
+      error2[i+1] = Error
+      error3[i+1] = Error
       
       sigma2_1[i+1] = a0 + a1*(error1[i]^2) + b1*(sigma2_1[i]^2)
       sigma2_2[i+1] = a0 + a1*(error2[i]^2) + b1*(sigma2_2[i]^2)
@@ -797,12 +753,95 @@ sum_var =summary(varfit)
 sum_var$varresult$x1
 
 coef.transfer_entropy(A,B)
+-7.560+.8136
 
-
-# SIMULACION SISTEMA 1
-Database <- Sistema_10(500)
-grafica_3var(Database,"10")
+# SIMULACION SISTEMA 9
+#Database <- Sistema_9(512)
+Database1 <- Sistema_9(1100)
+Database2<- Sistema_9(1000)
+Database2[,1] = Database2[,1] + tail(Database[,1],1)
+Database2[,2] = Database2[,2] + tail(Database[,2],1)
+Database2[,3] = Database2[,3] + tail(Database[,3],1)
+Database<-rbind(Database,Database2)
+Database<-tail(Database,2048)
+grafica_3var(Database,"9")
 Wavelet_3(Database)
 TE3_boot(Database)
 TE3_sbt(Database)
+
+fwrite(Database,file="F:/Tarea/TESIS/IMAGENES/Sistema_9_512.csv")
+
+
+
+
+
+
+############### Ejemplo 1
+
+Ejemplo_1 <- function(semilla = 12345){
+  tiempo = seq(0,50,by=1/12)
+  y_t=c()
+  set.seed(semilla)
+  for (i in 1:length(tiempo)) {
+    p1=10
+    if (tiempo[i] >= 20 && tiempo[i] <= 30) {
+      p2 = 5
+      y_t[i]=cos(2*pi*tiempo[i]/p1) + cos(2*pi*tiempo[i]/p2) + runif(1)
+    }else{
+      p2 = 3
+      y_t[i]=cos(2*pi*tiempo[i]/p1) + cos(2*pi*tiempo[i]/p2) + runif(1)
+    }
+  }
+  return(y_t)
+}
+
+
+Database<-Ejemplo_1()
+
+
+plot(Database,type= "l")
+A= cbind(rep(1:length(Database))/12,Database)
+
+
+
+
+
+
+
+
+
+############### Ejemplo 2
+
+
+Ejemplo_2 <- function(semilla = 12345){
+  tiempo = seq(0,50,by=1/12)
+  
+  y_t=c()
+  x_t=c()
+  set.seed(semilla)
+  for (i in 1:length(tiempo)) {
+    if(tiempo[i]<=25){
+      x_t[i] = 4*sin((2*pi/3) * (tiempo[i]+5/12)) - 3*sin((2*pi/6)*(tiempo[i] -10/12)) + runif(1)
+      
+      y_t[i] = sin(2*pi*tiempo[i]/3) + 3*sin(2*pi*tiempo[i]/6) + runif(1)
+      
+    }else{
+      x_t[i] = 4*sin((2*pi/3) * (tiempo[i]-5/12)) - 3*sin((2*pi/6)*(tiempo[i] + 10/12)) + runif(1)
+        
+      y_t[i] = sin(2*pi*tiempo[i]/3) + 3*sin(2*pi*tiempo[i]/6) + runif(1)
+      
+    }
+  }
+  return(list(x_t,y_t))
+}
+Ejemplo_2()
+
+
+
+
+
+
+
+
+
 
